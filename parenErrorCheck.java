a164060f422445ea6155a64cedb159b3b0361d3f
hehgendary <149680093+hehGendary@users.noreply.github.com>
2023-12-01 12:28:33 +0300

    parenErrorCheck.java

--- parenErrorCheck.java
+++ parenErrorCheck.java
@@ -1,32 +1,37 @@
-import java.util.Arrays;
-
 public class parenErrorCheck {
     String stringForCheck;
-    String[] openParens = {"(", "[", "{"};
-    String[] closeParens = {")", "]", "}"};
+    String openParens = "([{";
+    String closeParens = "}])";
 
     public parenErrorCheck(String stringForCheck) {
         this.stringForCheck = stringForCheck;
     }
 
     private boolean inOpenParens(char current) {
-        return Arrays.stream(openParens).toList().contains(current);
+        return openParens.indexOf(current) != -1;
     }
 
     private boolean inCloseParens(char current) {
-        return Arrays.stream(closeParens).toList().contains(current);
+        return closeParens.indexOf(current) != -1;
     }
 
     public boolean check() {
         int parensNumber = 0;
+        boolean inString = false;
 
         for (int pos = 0; pos < stringForCheck.length(); pos++) {
             char current = stringForCheck.charAt(pos);
 
-            if (inOpenParens(current)) {
+            if (current == '"') {
+                inString = !inString;
+            }
+
+            if (inOpenParens(current) && !inString) {
                 parensNumber++;
-            } else if (inCloseParens(current)) {
+
+            } else if (inCloseParens(current) && !inString) {
                 parensNumber = parensNumber - 1;
+                
                 if (parensNumber < 0) {
                     return false;
                 }
